4ccf2bf791d06aefed7b028f7cc75d79aad285de
이용준 <yongjun.lee@probit.com>
2021-05-04 16:25:04 +0900

    log 미들웨어 추가, static dir설정

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import path from "path";
+import logger from "morgan";
 
 import App from "./config/app";
 import apiRouter from "./router/v1";
@@ -9,6 +11,9 @@ const app = App.getApplication();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "assets")));
+
+app.use(logger("dev"));
 
 app.get("/", (req: express.Request, res: express.Response) => {
   console.log(req.params);
